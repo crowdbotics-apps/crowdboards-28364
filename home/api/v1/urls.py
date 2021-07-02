92d04447edf2c427096ab0ca29e6f014b1ac2c12
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-07-02 03:00:58 +0300

    settings config fix
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- home/api/v1/urls.py
+++ home/api/v1/urls.py
@@ -4,10 +4,14 @@ from rest_framework.routers import DefaultRouter
 from home.api.v1.viewsets import (
     ApplicationsViewSet,
     PlansViewSet,
+    SignupViewSet,
+    LoginViewSet,
     SubscriptionsViewSet,
 )
 
 router = DefaultRouter()
+router.register("signup", SignupViewSet, basename="signup")
+router.register("login", LoginViewSet, basename="login")
 router.register("app", ApplicationsViewSet, basename="apps")
 router.register("subscriptions", SubscriptionsViewSet, basename="subscriptions")
 router.register("plans", PlansViewSet, basename="plans")
--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+settings.configure()
 
 class Applications(models.Model):
     name = models.CharField(max_length=50)
